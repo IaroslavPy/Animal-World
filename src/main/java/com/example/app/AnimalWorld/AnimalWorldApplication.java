0079e468ee6480463aea8b2ae3d3bd9d6630f2d7
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-04 08:44:11 +0300

    Initial commit

---
+++ src/main/java/com/example/app/AnimalWorld/AnimalWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.app.AnimalWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalWorldApplication.class, args);
+	}
+
+}
